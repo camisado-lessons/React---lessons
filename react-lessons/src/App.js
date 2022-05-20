ab5d5f72156ecc915f9eea2edc4b7dd23ca93ae4
Maxim Sudakov <grax922@gmail.com>
2022-05-20 22:23:07 +0300

    refactor: task-4

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -5,13 +5,25 @@ function App() {
   const [checked, setChecked] = useState(false);
   let message;
   if(checked) {
-    message = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat et quo adipisci fuga aliquid. Quibusdam, repudiandae sunt inventore quidem ea aspernatur totam est numquam saepe autem sint rem odio cum?';
+    message = <div>
+      <h2>Ура, вам уже есть 18</h2>
+      <p>здесь расположен контент только для взрослых</p>
+    </div>
+  } else {
+    message = <div>
+      <p>
+        увы, вам еще нет 18 лет:(
+      </p>
+    </div>
   }
 
   return (
     <div className="App">
-      <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
-      <p>{message}</p>
+      <label>
+        Вам есть 18 лет?
+        <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
+      </label>  
+      {message}
     </div>
   );
 }
